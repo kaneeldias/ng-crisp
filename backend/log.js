710b7f0681510e7fd52ec4e8cbe2f97322374df4
Kaneel Dias <kaneeldias@gmail.com>
2019-05-06 13:52:01 +0530

    dfsd

--- backend/log.js
+++
@@ -1,6 +0,0 @@
-var log = function(msg){
-    var date = new Date();
-    console.log(new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().replace("T", " ").replace("Z", "") + " " + msg);
-}
-
-module.exports = { log };
--- src/app/config.ts
+++
@@ -1,9 +0,0 @@
-const url = "http://localhost:3000/apis";
-
-export class API{
-
-    public static getUrl(params:string){
-        return url+"/"+params;
-    }
-
-}
